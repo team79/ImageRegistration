4d09269d260ae9596c5352ce9a4437dbc6b59ba3
hzh <huzihao79@163.com>
2017-07-14 16:46:51 +0800

    hzh

--- ImageRegistration/ImageRegistration.h
+++ ImageRegistration/ImageRegistration.h
@@ -1,1 +1,6 @@
 #pragma once
+#include "opencv2/core/core.hpp"  
+#include "opencv2/features2d/features2d.hpp"  
+#include <opencv2/nonfree/nonfree.hpp>  
+#include "opencv.hpp"
+#include "highgui.h"
